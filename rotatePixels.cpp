135521931fe2ad0acee6e738f0c1472358058f74
squarekyzhong <tsinghuamit@sina.com>
2022-02-19 21:15:32 -0500

    fourteenth

--- rotatePixels.cpp
+++ rotatePixels.cpp
@@ -11,39 +11,12 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include<bits/stdc++.h>
 using namespace std;
 
-template<typename T>
-class stack{
-    private:
-          T arr[];
-          int i=0;
-          int compacity;
-    public:
-          stack(int length){
-              arr=new T[length];
-              compacity = length;
-          }
-          void push(T a){
-              i++;
-              arr[i] = a;
-          }
-          T pop(){
-              if(i>0){
-                int ret = arr[i];
-                i--;
-                return ret;  
-              }
-              else{
-                  return -1;
-              }
-          }
-};
-
 void rotatePixel(size_t d, int* arr);
 int main()
 {
     cout <<"matrix" <<endl;
     int arr[3][3]={{1, 2, 3},{4, 5, 6},{7, 8, 9}};
-    rotatePixel(3, &arr[0][0]);
+    rotatePixel(3, &arr[0][0]);//get the address of the first element.
     for(int i=0; i < 3;i++){
         for(int j=0; j < 3; j++){
             cout << arr[i][j] << '\t';
